c600ee974ca6b26869d3dea6a7d7add57555986d
Raymond Liu <raymond.kh.liu@gmail.com>
2024-10-08 09:19:01 -0400

    completed importable module

---
+++ wildlife_tracker/migration_tracking/migration_path.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+from wildlife_tracker.habitat_management.habitat import Habitat
+
+class MigrationPath:
+
+    def __init__(self,
+                destination: Habitat,
+                path_id: int,
+                start_location: Habitat,
+                duration: Optional[int] = None) -> None:
+        self.destination = destination
+        self.duration = duration
+        self.path_id = path_id
+        self.start_location = start_location
+
+    def get_migration_paths(self) -> list[MigrationPath]:
+        pass
+
+    def get_migration_path_details(self) -> dict:
+        pass
+
+    def update_migration_path_details(self, **kwargs) -> None:
+        pass
